71a48c02559678507c7f72d2ed9d80d6693ada6e
NatanyaH <natz.hrv@gmail.com>
2021-09-12 14:41:53 -0400

    Added date and time format

--- src/app.js
+++ src/app.js
@@ -1,3 +1,50 @@
+function formatDate(timeStamp) {
+  console.log(timeStamp);
+  let date = new Date(timeStamp * 1000);
+
+  let daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  console.log(date);
+  console.log(date.getDay());
+  console.log(date.getHours());
+  console.log(date.getMinutes());
+  console.log(date.getMonth());
+
+  let day = daysOfWeek[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let timeOfDay = "";
+
+  // calculate am or pm
+  if (hours < 12) {
+    timeOfDay = "am";
+  } else {
+    timeOfDay = "pm";
+  }
+
+  // calculate hours less than 10
+  if (hours < 10) {
+    hours = "0" + hours;
+  } else if (hours > 12) {
+    hours = hours - 12;
+  }
+
+  // calculate mins less than 10
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  return `${day}, ${hours}:${minutes} ${timeOfDay}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
 
@@ -12,12 +59,16 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let descriptionElement = document.querySelector("#weather-description");
+  let dateElement = document.querySelector("#date");
+
+  console.log(dateElement);
 
   temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = city;
   humidityElement.innerHTML = humidity;
   windElement.innerHTML = Math.round(wind);
   descriptionElement.innerHTML = description;
+  dateElement.innerHTML = formatDate(response.data.dt);
 }
 
 let cityName = "Montreal";
